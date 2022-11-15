3e264c3d6a5ee4b2f30544f48feddc719e676a2b
Kishor Kapale <81122744+kkapale@users.noreply.github.com>
2022-11-15 12:50:41 -0600

    Update tensorflow2_mnist_hvd.py

--- 06_distributedTraining/homework/tensorflow2_mnist_hvd.py
+++ 06_distributedTraining/homework/tensorflow2_mnist_hvd.py
@@ -198,7 +198,7 @@ for ep in range(args.epochs):
 
     tt1 = time.time()
     print('E[%d], train Loss: %.6f, training Acc: %.3f, val loss: %.3f, val Acc: %.3f\t Time: %.3f seconds' % (ep, training_loss, training_acc, test_loss, test_acc, tt1 - tt0))
-    metrics['Epochs'].append(ep.numpy())
+    metrics['Epochs'].append(ep)
     metrics['train_acc'].append(training_acc.numpy())
     metrics['train_loss'].append(training_loss.numpy())
     metrics['valid_acc'].append(test_acc.numpy())
